43bb44aa7d50b4ea28b24240aed0e79c3b36f2b9
phj799 <phj799@naver.com>
2023-03-08 17:02:21 +0900

    1일차 온.오프라인 수업내용 정리

---
+++ offline/day1/vue-test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>하이~</h1>
+    <AppChild />
+  </div>
+</template>
+
+<script>
+import AppChild from "./components/AppChild.vue"
+export default {
+  components: {
+    AppChild
+  }  
+}
+</script>
+
+
+<style>
+
+</style>

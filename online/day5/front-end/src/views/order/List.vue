3b3aaf7651caee1993fbacae25c2a36837cf232b
phj799 <phj799@naver.com>
2023-03-15 16:03:54 +0900

    vuejs 5일차 수업내용 정리

---
+++ online/day5/front-end/src/views/order/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>주문리스트</h1>
+
+    <ul>
+      <li v-for="order in orders" :key="order.id">{{order.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+import { api } from "@/utils/axios";
+
+export default {
+  data() {
+    return {
+      orders: [],
+    };
+  },
+  async created() {
+
+    const result = await api.orders.findAll();
+    this.orders = result.data;
+  },
+};
+</script>
+</script>
+
+<style scoped>
+
+</style>
